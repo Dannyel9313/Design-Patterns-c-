a6620cfe3aba0fb08871a4ae04fc0c60ba30a547
Daniel Sakalev <daniel.sakalev@egt-bg.com>
2020-04-05 19:40:19 +0300

    added cmake file to pimpl project

---
+++ Pimpl_Idiom/CMakeFiles/pimpl.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/default/DEV/DesignPatternRepo/Design-Patterns-c-/Pimpl_Idiom/Person.cpp" "/home/default/DEV/DesignPatternRepo/Design-Patterns-c-/Pimpl_Idiom/CMakeFiles/pimpl.dir/Person.cpp.o"
+  "/home/default/DEV/DesignPatternRepo/Design-Patterns-c-/Pimpl_Idiom/pimpl_idiom.cpp" "/home/default/DEV/DesignPatternRepo/Design-Patterns-c-/Pimpl_Idiom/CMakeFiles/pimpl.dir/pimpl_idiom.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Pimpl_Idiom/CMakeFiles/pimpl.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pimpl.dir/pimpl_idiom.cpp.o"
+  "CMakeFiles/pimpl.dir/Person.cpp.o"
+  "pimpl.pdb"
+  "pimpl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pimpl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
